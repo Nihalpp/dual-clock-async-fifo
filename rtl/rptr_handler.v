e86f9a79da5f7dce8dbf33054835dae329b55d0a
Muhammed Nihal PP <75819489+Nihalpp@users.noreply.github.com>
2026-02-28 15:13:28 +0530

    Update rptr_handler.v

--- rtl/rptr_handler.v
+++ rtl/rptr_handler.v
@@ -1,1 +1,51 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 30.07.2025 21:15:34
+// Design Name: 
+// Module Name: rptr_handler
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
 
+module rptr_handler #(parameter PTR_WIDTH=3) (
+  input rclk, rrst_n, r_en,
+  input [PTR_WIDTH:0] g_wptr_sync,
+  output reg [PTR_WIDTH:0] b_rptr, g_rptr,
+  output reg empty
+);
+
+  reg [PTR_WIDTH:0] b_rptr_next;
+  reg [PTR_WIDTH:0] g_rptr_next;
+
+  assign b_rptr_next = b_rptr+(r_en & !empty);
+  assign g_rptr_next = (b_rptr_next >>1)^b_rptr_next;
+  assign rempty = (g_wptr_sync == g_rptr_next);
+  
+  always@(posedge rclk or negedge rrst_n) begin
+    if(!rrst_n) begin
+      b_rptr <= 0;
+      g_rptr <= 0;
+    end
+    else begin
+      b_rptr <= b_rptr_next;
+      g_rptr <= g_rptr_next;
+    end
+  end
+  
+  always@(posedge rclk or negedge rrst_n) begin
+    if(!rrst_n) empty <= 1;
+    else        empty <= rempty;
+  end
+endmodule
